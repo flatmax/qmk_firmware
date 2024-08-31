b2407c7090977069fe6014705bbd2ea0f8db991e
Matt Flax <flatmax@flatmax.com>
2024-08-31 21:14:21 +1000

    added control, arrows and brackets layer

--- keyboards/handwired/flatmax/keymaps/default/keymap.c
+++ keyboards/handwired/flatmax/keymaps/default/keymap.c
@@ -18,25 +18,25 @@
 
 enum layers {
   _BASELAYER,
-  _FUNCTIONLAYER
+  _CAB
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASELAYER] = LAYOUT(
-        KC_NO, KC_ESC,  KC_GRAVE, KC_1,	KC_2,  KC_3,  KC_4,  KC_5,      KC_6,   KC_7,   KC_8,   KC_9,  KC_0,   KC_MINUS,   KC_EQUAL, KC_PSCR,
-        KC_NO, KC_F3, KC_TAB,   KC_Q, KC_W,  KC_E,  KC_R,  KC_T,      KC_Y,   KC_U,   KC_I,   KC_O,  KC_P,   KC_LBRC,    KC_RBRC,  KC_BSLS,
-        KC_NO, KC_NO, KC_CAPS,  KC_A, KC_S,  KC_D,  KC_F,  KC_G,        KC_H,   KC_J,   KC_K,   KC_L,  KC_SCLN, KC_QUOT, KC_UP,  QK_BOOT,
-        KC_NO, KC_NO, KC_LCTL,  KC_Z, KC_X, KC_C, KC_V, KC_B,            KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RIGHT,
-    KC_NO, KC_NO, KC_NO, QK_BOOT, KC_LGUI, KC_LSFT, KC_SPC, KC_LALT,  KC_BSPC, KC_SPC, KC_ENTER, KC_PGDN, KC_PGUP,  KC_PGUP,  KC_NO,  KC_NO,
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_HOME, KC_END,   KC_DEL, KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO
+KC_NO, KC_ESC, KC_GRAVE, KC_1,	  KC_2,    KC_3,    KC_4,    KC_5,     KC_6,       KC_7,    KC_8,     KC_9,    KC_0,    KC_MINUS, KC_EQUAL, KC_PSCR,
+KC_NO, KC_F3,  KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_Y,       KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC,  KC_RBRC,  KC_BSLS,
+KC_NO, KC_NO,  KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_H,       KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,  KC_UP,    QK_BOOT,
+KC_NO, KC_NO,  MO(_CAB), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_N,       KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_LEFT,  KC_DOWN,  KC_RIGHT,
+KC_NO, KC_NO,  KC_NO,    QK_BOOT, KC_LGUI, KC_LSFT, KC_SPC,  KC_LALT,  KC_BSPC,    KC_SPC,  KC_ENTER, KC_PGDN, KC_PGUP, KC_PGUP,  KC_NO,    KC_NO,
+KC_NO, KC_NO,  KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_HOME, KC_END,   KC_DEL,     KC_LSFT, KC_NO,    KC_NO,   KC_NO,   KC_NO,    KC_NO,    KC_NO
     ),
 
-    [_FUNCTIONLAYER] = LAYOUT(
-      KC_NO, KC_NO, KC_GRAVE, KC_1,	KC_2,  KC_3,  KC_4,  KC_5,      KC_6,   KC_7,   KC_8,   KC_9,  KC_0, KC_MINUS,   KC_EQUAL, KC_BSPC,
-      KC_NO, KC_NO, KC_TAB,   KC_Q, KC_W,  KC_E,  KC_R,  KC_T,      KC_Y,   KC_U,   KC_I,   KC_O,  KC_P, KC_LBRC,    KC_RBRC,  KC_BSLS,
-      KC_NO, KC_NO, KC_CAPS,  KC_A, KC_S,  KC_D,  KC_F,  KC_G,      KC_H,   KC_J,   KC_K,   KC_L,  KC_SCLN, KC_QUOT, KC_UP,    KC_NO,
-      KC_NO, KC_NO, KC_LCTL,  KC_Z, KC_X, KC_C, KC_V, KC_B,         KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LEFT, KC_DOWN, KC_RIGHT,
-      KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_SPC, KC_HOME, KC_LALT, KC_END,   KC_DEL, KC_RALT, KC_BSPC, KC_SPC, KC_RSFT, KC_NO,  KC_NO,  KC_NO,
-      KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_SPC, KC_HOME, KC_LALT, KC_END,   KC_DEL, KC_RALT, KC_BSPC, KC_SPC, KC_RSFT, KC_NO,  KC_NO,  KC_NO
+    [_CAB] = LAYOUT(
+KC_NO, KC_NO, KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_NO, KC_MINUS,  KC_BSLS,     KC_EQUAL, KC_NO,   KC_NO, KC_NO, KC_NO,
+KC_NO, KC_NO, C(KC_TAB), C(KC_Q), C(KC_W), C(KC_E), C(KC_R), C(KC_T),     KC_NO, KC_LBRC,   KC_QUOT,     KC_RBRC,  C(KC_P), KC_NO, KC_NO, KC_NO,
+KC_NO, KC_NO, KC_NO,     C(KC_A), C(KC_S), C(KC_D), C(KC_F), C(KC_G),     KC_NO, S(KC_9),   KC_UP,       S(KC_0),  KC_NO,   KC_NO, KC_NO, KC_NO,
+KC_NO, KC_NO, KC_NO,     C(KC_Z), C(KC_X), C(KC_C), C(KC_V), C(KC_B),     KC_NO, KC_LEFT,   KC_DOWN,     KC_RIGHT, KC_NO,   KC_NO, KC_NO, KC_NO,
+KC_NO, KC_NO, KC_NO, KC_NO, C(KC_LGUI), C(KC_LSFT), C(KC_SPC), C(KC_LALT),KC_NO, C(KC_SPC), C(KC_ENTER), KC_NO,    KC_NO,   KC_NO, KC_NO, KC_NO,
+KC_NO, KC_NO, KC_NO,     KC_NO,   KC_NO,   KC_NO,  C(KC_HOME), C(KC_END), KC_NO, KC_NO,     KC_NO,       KC_NO,    KC_NO,   KC_NO, KC_NO, KC_NO
     )
 };
