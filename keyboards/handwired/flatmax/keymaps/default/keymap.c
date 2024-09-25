0da536a5f7b97e728744496f2fb1b4619530fb57
Matt Flax <flatmax@flatmax.org>
2024-09-25 14:46:14 +1000

    trying a simpler keymap

--- keyboards/handwired/flatmax/keymaps/default/keymap.c
+++ keyboards/handwired/flatmax/keymaps/default/keymap.c
@@ -50,42 +50,23 @@
 
 enum layers {
   _BASELAYER,
-  _CTL,
-  _ALT,
-  _CTL_ALT
+  // _CTL,
+  _LAYER1
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASELAYER] = LAYOUT(
 KC_GRAVE, KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                  KC_Y,       KC_U,    KC_I,     KC_O,    KC_P,    KC_ESC,
 KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,                  KC_H,       KC_J,    KC_K,     KC_L,    KC_SCLN, KC_PSCR,
-MO(_CTL), KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,       KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_F3,
-KC_NO,    MO(_ALT), KC_LGUI, KC_LSFT, KC_SPC,  QK_BOOT,               KC_ENTER,   KC_SPC,  KC_PGUP, KC_PGDN,  KC_LSFT, KC_NO,
+KC_LCTL, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,       KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_F3,
+KC_NO,    KC_LALT, KC_LGUI, KC_LSFT, MO(_LAYER1),  QK_BOOT,          KC_ENTER,   KC_SPC,  KC_PGUP, KC_PGDN,  KC_LSFT, KC_NO,
 KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_HOME, KC_END,                KC_BSPC,    KC_DEL,  KC_NO,    KC_NO,   KC_NO,   KC_NO
     ),
-    [_CTL] = LAYOUT(
-KC_NO,     C(KC_Q), C(KC_W),    C(KC_E),    C(KC_R),    C(KC_T),      C(KC_Y),     C(KC_U),    C(KC_I),    C(KC_O),     C(KC_P),     QK_BOOT,
-C(KC_TAB), C(KC_A), C(KC_S),    C(KC_D),    C(KC_F),    C(KC_G),      C(KC_H),     C(KC_J),    C(KC_K),    C(KC_L),     C(KC_SCLN),  KC_NO,
-_______,   C(KC_Z), C(KC_X),    C(KC_C),    C(KC_V),    C(KC_B),      C(KC_SLSH),  C(KC_LEFT), C(KC_UP),   C(KC_DOWN),  C(KC_RIGHT), KC_NO,
-KC_NO,     _______, C(KC_LGUI), C(KC_LSFT), C(KC_SPC),  KC_NO,        C(KC_ENTER), C(KC_SPC),  C(KC_PGUP), C(KC_PGDN),  KC_LSFT,     KC_NO,
-KC_NO,     KC_NO,   KC_NO,      KC_NO,      C(KC_HOME), C(KC_END),    KC_BSPC,     KC_DEL,     KC_NO,      KC_NO,       KC_NO,       KC_NO
-    ),
-    [_ALT] = LAYOUT(
-KC_NO,     A(KC_Q), KC_7,  KC_8,       KC_9,       A(KC_T),           S(KC_LBRC),  KC_MINUS,    KC_BSLS, S(KC_EQUAL), S(KC_RBRC), KC_NO,
-A(KC_TAB), A(KC_A), KC_4,  KC_5,       KC_6,       A(KC_G),           KC_LBRC,     S(KC_MINUS), KC_QUOT, KC_EQUAL,    KC_RBRC,    KC_NO,
-_______,   A(KC_Z), KC_1,	 KC_2,       KC_3,       A(KC_B),           A(KC_LEFT),  KC_LEFT,     KC_UP,   KC_DOWN,     KC_RIGHT,   KC_NO,
-KC_NO,     KC_NO,   KC_NO, KC_NO,      A(KC_HOME), A(KC_END),         KC_BSPC,     KC_DEL,      KC_NO,   KC_NO,       KC_NO,      KC_NO
-KC_NO,     _______, KC_0,  A(KC_LSFT), A(KC_SPC),  KC_NO,             A(KC_ENTER), A(KC_SPC),   S(KC_9), S(KC_0),     A(KC_LSFT), KC_NO,
-    ),
-    [_CTL_ALT] = LAYOUT(
-KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO, C(A(KC_T)),                  KC_NO, KC_NO,         KC_NO,       KC_NO,         KC_NO,          KC_NO,
-KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,                       KC_NO, KC_NO,         KC_NO,       KC_NO,         KC_NO,          KC_NO,
-_______, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,                       KC_NO, C(A(KC_LEFT)), C(A(KC_UP)), C(A(KC_DOWN)), C(A(KC_RIGHT)), KC_NO,
-KC_NO, _______,   KC_NO, KC_LSFT, KC_NO, KC_NO,                       KC_NO, KC_NO,         KC_NO,       KC_NO,         KC_LSFT,        KC_NO,
-KC_NO,   KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,                       KC_NO, KC_NO,         KC_NO,       KC_NO,         KC_NO,          KC_NO
+    [_LAYER1] = LAYOUT(
+_______, _______, KC_7,    KC_8,  KC_9,     KC_T,           S(KC_LBRC),  KC_MINUS,    KC_BSLS, S(KC_EQUAL), S(KC_RBRC), QK_BOOT,
+_______, _______, KC_4,    KC_5,  KC_6,     KC_G,           KC_LBRC,     S(KC_MINUS), KC_QUOT, KC_EQUAL,    KC_RBRC,    KC_NO,
+_______, _______, KC_1,	   KC_2,  KC_3,     KC_B,           A(KC_LEFT),  KC_LEFT,     KC_UP,   KC_DOWN,     KC_RIGHT,   KC_NO,
+KC_NO,   _______, _______, KC_0,  _______,  KC_NO,          _______,     _______,     S(KC_9), S(KC_0),     _______,    KC_NO,
+KC_NO,   KC_NO,   KC_NO,   KC_NO, _______,  _______,        _______,     _______,     KC_NO,   KC_NO,       KC_NO,      KC_NO
     )
 };
-
-layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _CTL, _ALT, _CTL_ALT);
-}
