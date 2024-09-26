38b956407f06a9365b7a7d4641112dd8e401401e
Matt Flax <flatmax@flatmax.org>
2024-09-27 09:18:06 +1000

    updated keymap targeting a 3 thumb cluster

--- keyboards/handwired/flatmax/keymaps/default/keymap.c
+++ keyboards/handwired/flatmax/keymaps/default/keymap.c
@@ -56,17 +56,17 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASELAYER] = LAYOUT(
-KC_GRAVE, KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                  KC_Y,       KC_U,    KC_I,     KC_O,    KC_P,    KC_ESC,
-KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,                  KC_H,       KC_J,    KC_K,     KC_L,    KC_SCLN, KC_PSCR,
-KC_LCTL, KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,       KC_M,    KC_COMM,  KC_DOT,  KC_SLSH, KC_F3,
-KC_NO,    KC_LALT, KC_LGUI, KC_LSFT, MO(_LAYER1),  QK_BOOT,          KC_ENTER,   KC_SPC,  KC_PGUP, KC_PGDN,  KC_LSFT, KC_NO,
-KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_HOME, KC_END,                KC_BSPC,    KC_DEL,  KC_NO,    KC_NO,   KC_NO,   KC_NO
+KC_GRAVE, KC_Q,     KC_W,    KC_E,    KC_R,        KC_T,                KC_Y,       KC_U,    KC_I,    KC_O,    KC_P,    KC_ESC,
+KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,        KC_G,                KC_H,       KC_J,    KC_K,    KC_L,    KC_SCLN, KC_PSCR,
+KC_LCTL,  KC_Z,     KC_X,    KC_C,    KC_V,        KC_B,                KC_N,       KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_F3,
+KC_NO,    KC_LALT,  KC_LGUI, KC_LSFT, MO(_LAYER1), KC_BSPC,             KC_ENTER,   KC_SPC,  KC_PGUP, KC_PGDN, KC_LSFT, KC_NO,
+KC_NO,    KC_NO,    KC_NO,   KC_NO,   KC_NO,       KC_NO,               KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
     ),
     [_LAYER1] = LAYOUT(
-_______, _______, KC_7,    KC_8,  KC_9,     KC_T,           S(KC_LBRC),  KC_MINUS,    KC_BSLS, S(KC_EQUAL), S(KC_RBRC), QK_BOOT,
-_______, _______, KC_4,    KC_5,  KC_6,     KC_G,           KC_LBRC,     S(KC_MINUS), KC_QUOT, KC_EQUAL,    KC_RBRC,    KC_NO,
-_______, _______, KC_1,	   KC_2,  KC_3,     KC_B,           A(KC_LEFT),  KC_LEFT,     KC_UP,   KC_DOWN,     KC_RIGHT,   KC_NO,
-KC_NO,   _______, _______, KC_0,  _______,  KC_NO,          _______,     _______,     S(KC_9), S(KC_0),     _______,    KC_NO,
+QK_BOOT, _______, KC_7,    KC_8,  KC_9,     _______,        S(KC_LBRC),  KC_MINUS,    KC_BSLS, S(KC_EQUAL), S(KC_RBRC), QK_BOOT,
+_______, _______, KC_4,    KC_5,  KC_6,     KC_HOME,        KC_LBRC,     S(KC_MINUS), KC_QUOT, KC_EQUAL,    KC_RBRC,    KC_NO,
+_______, _______, KC_1,	   KC_2,  KC_3,     KC_END,         _______,     KC_LEFT,     KC_UP,   KC_DOWN,     KC_RIGHT,   KC_NO,
+KC_NO,   _______, _______, KC_0,  _______,  KC_DEL,         _______,     _______,     S(KC_9), S(KC_0),     _______,    KC_NO,
 KC_NO,   KC_NO,   KC_NO,   KC_NO, _______,  _______,        _______,     _______,     KC_NO,   KC_NO,       KC_NO,      KC_NO
     )
 };
